718bbea040ea90a1d40e6ab29c452577b64bd5ae
gaurabgi <127061048+gaurabgi@users.noreply.github.com>
2023-10-13 12:15:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("my first consol project");
+
+var first_name="gaurangi";
+var last_name="aggarwal";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
+
+var sud = a - b;
+console.log(sud);
+
+var mull = a * b;
+console.log(mull);
+
+
+var div = a/b; 
+console.log(div);
